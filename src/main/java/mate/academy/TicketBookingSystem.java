5e00c2d4618a1825e8b45605aff005791583f68f
alex <ursusandwolf@protonmail.com>
2026-03-17 19:09:04 +0200

    release

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -15,15 +15,15 @@ public class TicketBookingSystem {
         try {
             if (tickets.tryAcquire(100, TimeUnit.MILLISECONDS)) { // пытаемся купить билет
                 System.out.println(user + ": The ticket acquired!");
-                return new BookingResult(user, true, "The ticket acquired!");
+                Thread.sleep(10);
+                tickets.release();
+                return new BookingResult(user, true, "Booking successful.");
             } else {
                 System.out.println(user + ": No free seats!");
-                return new BookingResult(user, false, "No free seats!");
+                return new BookingResult(user, false, "No seats available.");
             }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
-        } finally {
-            tickets.release();
         }
     }
 }
