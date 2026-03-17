38e04b131fb37d468ab41ead9fb7fba711b9b567
alex <ursusandwolf@protonmail.com>
2026-03-17 18:34:25 +0200

    out of memory (

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,22 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
 
-    public TicketBookingSystem(int totalSeats) {
+    private final Semaphore tickets;
 
+    public TicketBookingSystem(int totalSeats) {
+        tickets = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (tickets.tryAcquire()) { // пытаемся купить билет
+            System.out.println("Клиент " + user + " купил билет");
+            return new BookingResult(user, true, "The ticket acquired!");
+        } else {
+            System.out.println("Клиент " + user + " НЕ смог купить билет (нет мест)");
+            return new BookingResult(user, false, "No free seats!");
+        }
     }
 }
