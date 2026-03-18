90bec9decb63f18003c8fef2b738bb7b205239b7
alex <ursusandwolf@protonmail.com>
2026-03-18 13:46:53 +0200

    Callable

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,29 +1,61 @@
 package mate.academy;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 public class TicketBookingSystem {
 
     private final Semaphore tickets;
+    private final ExecutorService executor;
 
     public TicketBookingSystem(int totalSeats) {
         tickets = new Semaphore(totalSeats);
+        executor = Executors.newFixedThreadPool(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
+        Customer customer = new Customer(user, tickets);
+        Future<Boolean> future = executor.submit(customer);
         try {
-            if (tickets.tryAcquire(100, TimeUnit.MILLISECONDS)) { // пытаемся купить билет
-                System.out.println(user + ": The ticket acquired!");
-                Thread.sleep(10);
-                tickets.release();
+            if (future.get()) {
                 return new BookingResult(user, true, "Booking successful.");
             } else {
-                System.out.println(user + ": No free seats!");
                 return new BookingResult(user, false, "No seats available.");
             }
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public class Customer implements Callable<Boolean> {
+        private String name;
+        private Semaphore semaphore;
+
+        public Customer(String name, Semaphore semaphore) {
+            this.name = name;
+            this.semaphore = semaphore;
+        }
+
+        @Override
+        public Boolean call() throws InterruptedException {
+            boolean acquired = semaphore.tryAcquire(2, TimeUnit.SECONDS);
+            if (!acquired) {
+                System.out.println(name + " failed to acquire");
+                return false;
+            }
+            try {
+                System.out.println(name + " acquired the access");
+                // critical section
+                return true;
+            } finally {
+                System.out.println(name + " is going to release the access");
+                semaphore.release();
+            }
         }
     }
 }
